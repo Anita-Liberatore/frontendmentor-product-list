df8ba29b0af7cb81bf7a48d40330243e7ca5be4e
Anita Liberatore <anitaliberatore@hotmail.com>
2025-08-30 01:07:12 +0200

    fix images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets/images/*', dest: 'images' } // -> dist/images/...
+      ]
+    })
+  ],
+  // se pubblichi su sotto-path (GitHub Pages):
+  // base: '/nome-repo/'
 })
